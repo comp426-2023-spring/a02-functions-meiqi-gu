79a83db2bf9098a9b806c7f7262adb48fea85a50
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-04-15 17:47:39 -0400

    add help to cli.sh

--- cli.sh
+++ cli.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env node
+#Get help 
+show_help() {
+    printf -- "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE"
+    printf -- "\n"
+    printf -- "  -h\t\tShow this help message and exit.\n"
+    printf --  "  -n, -s\tLatitude: N positive; S negative.\n"
+    printf -- "  -e, -w\tLongitude: E positive; W negative.\n"
+    printf -- "  -z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n"
+    printf -- "  -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
+    printf -- "  -j\t\tEcho pretty JSON from open-meteo API and exit.\n"
+    exit 0
+}
