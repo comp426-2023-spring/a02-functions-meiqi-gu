e2ad7d34bb0ff1263223c5529de9bb66eb16e32a
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-04-15 18:03:23 -0400

    response text copied from assignment README

--- cli.sh
+++ cli.sh
@@ -15,3 +15,13 @@ show_help() {
 const timezone = moment.tz.guess();
 const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latittude + '&longitude=-' + longitutde + '&daily=precipitation_hours&current_weather=true&timezone=America%2FNew_York'); 
 const data = await response.json(); 
+
+const days = args.d; 
+
+if (days == 0) {
+  console.log("today.")
+} else if (days > 1) {
+  console.log("in " + days + " days.")
+} else {
+  console.log("tomorrow.")
+}
