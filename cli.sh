63198296d35fef09b5983ba41fdaa2982a834cfe
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-02 20:01:08 -0400

    chg main: index.js -> cli.js

--- cli.sh
+++ cli.sh
@@ -4,7 +4,7 @@ show_help() {
     printf -- "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE"
     printf -- "\n"
     printf -- "  -h\t\tShow this help message and exit.\n"
-    printf --  "  -n, -s\tLatitude: N positive; S negative.\n"
+    printf -- "  -n, -s\tLatitude: N positive; S negative.\n"
     printf -- "  -e, -w\tLongitude: E positive; W negative.\n"
     printf -- "  -z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n"
     printf -- "  -d 0-6\tDay to retrieve weather: 0 is today; defaults to 1.\n"
