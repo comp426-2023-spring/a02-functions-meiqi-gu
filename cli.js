2a47bc7d5844d6d3ba9353d3854f1fcb3f8ecb5d
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-05 23:01:07 -0400

    add fetch/response

--- cli.js
+++ cli.js
@@ -27,6 +27,7 @@ console.log(latitude);
 console.log(longitude); 
 
 
-//const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone='+timezone);
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+latitude+'&longitude='+longitude+'&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone='+timezone);
 
-//const data = await response.json(); 
+const data = await response.json(); 
+console.log(data); 
