0ecb5cbdb522057cae9b61996c2e46ee99bb95ed
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-05 22:42:51 -0400

    add timezone

--- cli.js
+++ cli.js
@@ -1,13 +1,22 @@
-#!/usr/bin/env node
-// Get help 
-
+#!/usr/bin/env node 
+/* Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE
+    -h            Show this help message and exit.
+    -n, -s        Latitude: N positive; S negative.
+    -e, -w        Longitude: E positive; W negative.
+    -z            Time zone: uses tz.guess() from moment-timezone by default.
+    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.
+    -j            Echo pretty JSON from open-meteo API and exit.
+*/ 
 import minimist from 'minimist'; 
-import moment from 'moment'; 
+import moment from 'moment-timezone'; 
 
 const mini = minimist(process.argv.slice(2)); 
 const helpText = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n\t-h \t\tShow this help message and exit.\n\t-n, -s\t\tLatitude: N positive; S negative.\n\t-e, -w\t\tLongitude: E positive; W negative.\n\t-z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n\t-d 0-6\t\tDay to retrieve weather: 0 is today; defaults to 1.\n\t-j\t\tEcho pretty JSON from open-meteo API and exit.\n"; 
 if (mini['h']) {
   console.log(helpText); 
+  process.exit(0); 
 }
 
 const timezone = moment.tz.guess(); 
+console.log(timezone); 
+//const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude='+LATITUDE+'&longitude='+LONGITUTDE+'&daily=precipitation_hours&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone='+timezone);
