6eae128ec1013ae04cee9c1c1fc38e75bc08233f
meiqi-gu <77755155+meiqi-gu@users.noreply.github.com>
2023-05-05 22:28:04 -0400

    reset; create help txt

--- cli.js
+++ cli.js
@@ -1,17 +1,9 @@
 #!/usr/bin/env node
 // Get help 
 
-
-const timezone = moment.tz.guess();
-const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latittude + '&longitude=-' + longitutde + '&daily=precipitation_hours&current_weather=true&timezone=America%2FNew_York'); 
-const data = await response.json(); 
-
-const days = args.d; 
-
-if (days == 0) {
-  console.log("today.")
-} else if (days > 1) {
-  console.log("in " + days + " days.")
-} else {
-  console.log("tomorrow.")
+import minimist from 'minimist'; 
+const mini = minimist(process.argv.slice(2)); 
+const helpText = "Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE\n\t-h \t\tShow this help message and exit.\n\t-n, -s\t\tLatitude: N positive; S negative.\n\t-e, -w\t\tLongitude: E positive; W negative.\n\t-z\t\tTime zone: uses tz.guess() from moment-timezone by default.\n\t-d 0-6\t\tDay to retrieve weather: 0 is today; defaults to 1.\n\t-j\t\tEcho pretty JSON from open-meteo API and exit.\n"; 
+if (mini['h']) {
+  console.log(helpText); 
 }
